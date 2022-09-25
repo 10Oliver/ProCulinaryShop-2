12a60d93c88279eb17093a3a25464ff12e7f07d4
10Oliver <oliver.erazo1@gmail.com>
2022-09-24 18:36:03 -0600

    Trabajando en el proceso de validación de datos personales (correo) para una contraseña segura

--- api/helpers/verificador.php
+++ api/helpers/verificador.php
@@ -4,7 +4,8 @@
 * Clase que se encarga de validar los datos de las vistas hacia los modelos
 */
 
-class Verificador{
+class Verificador
+{
 
 
     // Propiedades para manejar algunas validaciones.
@@ -35,7 +36,7 @@ class Verificador{
     {
         return $this->fileError;
     }
-    
+
 
     /*
     * Función que recibe los datos de los formularios y elimina los 
@@ -53,7 +54,7 @@ class Verificador{
     }
 
 
-        /*
+    /*
     *   Método para validar un archivo de imagen.
     *
     *   Parámetros: $file (archivo de un formulario), $maxWidth (ancho máximo para la imagen) y $maxHeigth (alto máximo para la imagen).
@@ -97,7 +98,7 @@ class Verificador{
 
 
 
-        /*
+    /*
     *   Método para validar la ubicación de un archivo antes de subirlo al servidor.
     *
     *   Parámetros: $file (archivo), $path (ruta del archivo) y $name (nombre del archivo).
@@ -140,7 +141,7 @@ class Verificador{
             return false;
         }
     }
-        /*
+    /*
     *   Método para validar un número natural como por ejemplo llave primaria, llave foránea, entre otros.
     *
     *   Parámetros: $value (dato a validar).
@@ -150,21 +151,19 @@ class Verificador{
     public function validateNaturalNumber($value)
     {
         // Se verifica que el valor sea un número entero mayor o igual a uno.
-        if (is_numeric($value)) 
-        {   
+        if (is_numeric($value)) {
 
-            if((INT)$value>=0){
+            if ((int)$value >= 0) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
-        }else {
+        } else {
             return false;
-            
         }
     }
 
-      /*
+    /*
     *   Método para validar un correo electrónico.
     *
     *   Parámetros: $value (dato a validar).
@@ -180,7 +179,7 @@ class Verificador{
         }
     }
 
-        /*
+    /*
     *   Método para validar un dato booleano.
     *
     *   Parámetros: $value (dato a validar).
@@ -196,7 +195,7 @@ class Verificador{
         }
     }
 
-        /*
+    /*
     *   Método para validar una cadena de texto (letras, digitos, espacios en blanco y signos de puntuación).
     *
     *   Parámetros: $value (dato a validar), $minimum (longitud mínima) y $maximum (longitud máxima).
@@ -213,7 +212,7 @@ class Verificador{
         }
     }
 
-        /*
+    /*
     *   Método para validar un dato alfabético (letras y espacios en blanco).
     *
     *   Parámetros: $value (dato a validar), $minimum (longitud mínima) y $maximum (longitud máxima).
@@ -230,7 +229,7 @@ class Verificador{
         }
     }
 
-        /*
+    /*
     *   Método para validar un dato alfanumérico (letras, dígitos y espacios en blanco).
     *
     *   Parámetros: $value (dato a validar), $minimum (longitud mínima) y $maximum (longitud máxima).
@@ -247,7 +246,7 @@ class Verificador{
         }
     }
 
-        /*
+    /*
     *   Método para validar un dato monetario.
     *
     *   Parámetros: $value (dato a validar).
@@ -264,7 +263,7 @@ class Verificador{
         }
     }
 
-      /*
+    /*
     *   Método para validar una contraseña.
     *
     *   Parámetros: $value (dato a validar).
@@ -288,7 +287,7 @@ class Verificador{
         }
     }
 
-    
+
     /*
     *   Método para validar el formato del DUI (Documento Único de Identidad).
     *
@@ -306,7 +305,7 @@ class Verificador{
         }
     }
 
-        /*
+    /*
     *   Método para validar un número telefónico.
     *
     *   Parámetros: $value (dato a validar).
@@ -323,7 +322,7 @@ class Verificador{
         }
     }
 
-        /*
+    /*
     *   Método para validar una fecha.
     *
     *   Parámetros: $value (dato a validar).
@@ -352,7 +351,8 @@ class Verificador{
      * 
      */
 
-    public function encontrarPalabra($palabra, $clave) {
+    public function encontrarPalabra($palabra, $clave)
+    {
         //Se verifica la longitud de la palabra a buscar
         if (strlen($palabra) > 3) {
             //Si la palabra no es muy pequeña se inicia el proceso de coincidencias
@@ -362,7 +362,7 @@ class Verificador{
             //Se divide la clave en un arreglo
             $claveDividida = explode("", $clave);
             //Se procede a revisar todas las subcadenas obtenidas de la palabra de busqueda
-            foreach($palabraDividida AS $palabraActual) {
+            foreach ($palabraDividida as $palabraActual) {
                 //Se divide la palabra actual en un vector
                 $palabraBusqueda = explode("", $palabraActual);
                 /**
@@ -372,7 +372,18 @@ class Verificador{
                  * de la clave, y si este vuelve a encontrarse se vuelve a sumar otra coincidencia
                  * pero si ya no es la misma esta secuencia se rompe y se reiniciará las coincidencias a 0
                  */
-                for ($i=0; $i < count($palabraBusqueda); $i++) { 
+                for ($i = 0; $i < count($palabraBusqueda); $i++) {
+                    //Se revisa si el caracter de la palabra tiene una "i" o una "y" las cuales se tomarán como si
+                    //se tratara de un disminutivo de la palabra, (Es un caso excepcional)
+                    if (preg_match('[yYiI]', $palabraBusqueda[$i])) {
+                        if ($palabraBusqueda[$i] == $claveDividida[$i]) {
+                            //Se suman las coincidencias
+                            $coincidencias++;
+                        } else {
+                            $coincidencias = 0; //Se reinician las coincidencias
+                        }
+                    }
+
                     if ($palabraBusqueda[$i] == $claveDividida[$i]) {
                         //Se suman las coincidencias
                         $coincidencias++;
@@ -381,12 +392,50 @@ class Verificador{
                     }
                     /**
                      * Se revisa el total de coincidencias
+                     * Las cuales deben de ser de al menos 3
                      */
+                    if ($coincidencias >= 3) {
+                        return true;
+                        break;
+                    }
                 }
             }
+            //Si nunca llegó a las 3 o más coincidencias se toma comoque la palabra no se encuentra dentro de la clave
+            return false;
         }
     }
 
+    /**
+     * Función para validar que el correo no esté dentro de la contraseña
+     */
+
+     public function correoClave($correo, $clave) {
+        //Se revisa que el correo entero no esté dentro de la constraseña
+        if (!str_contains($clave, $correo)) {
+            return true;
+        } 
+        //Se divide el correo en dos partes
+        $nombreCorreo = "";
+        $dominio = "";
+     }
+
+
+    /**
+     * Función para darle una pista al usuario de su propio correo
+     */
+    public function formatEmail($correo)
+    {
+        //Se recorta las primeras 3 líneas del correo
+        $comienzo = substr($correo, 0, strlen($correo) - (strlen($correo) - 3));
+        //Se extraen el dominio del correo
+        $final = substr($correo, strripos($correo, '@') - strlen($correo));
+        //Se obtiene el sobrante del correo para saber su longitud
+        $restante = substr($correo, (strlen($correo) - (strlen($correo) - 3)), (strripos($correo, '@') - strlen($correo)));
+        //Se le agregan asteríscos según la longitud del correo restante
+        $total = str_pad($comienzo, strlen($restante) + 3, "*", STR_PAD_RIGHT);
+        //Se une el todo para generar el nuevo formato de correo
+        return $total . $final;
+    }
 
 
 
@@ -421,14 +470,16 @@ class Verificador{
             $this->passwordError = 'La contraseña debe de contener al menos una letra mayúscula';
         } elseif (!preg_match('[a-z]', $clave)) {
             $this->passwordError = 'La contraseña debe de contener al menos una letra minúscula';
-        } elseif(!preg_match('[0-9]', $clave)) {
+        } elseif (!preg_match('[0-9]', $clave)) {
             $this->passwordError = 'La contraseña debe de contener al menos un número';
-        } elseif(!preg_match('[áÁäÄéÉëËíÍïÏóÓöÖúÚüÜ!#$&/()=?¡!¿?*-+.,;:]', $clave)) {
+        } elseif (!preg_match('[áÁäÄéÉëËíÍïÏóÓöÖúÚüÜ!#$&/()=?¡!¿?*-+.,;:]', $clave)) {
             $this->passwordError = 'La contraseña debe de contener al menos un símbolo';
-        }
+        } elseif (!$this->encontrarPalabra($usuario, $clave)) {
+            $this->passwordError = 'Tú usuario o una fracción de él no puede ser parte de la contraseña';
+        } elseif (!$this->encontrarPalabra($nombre, $clave)) {
+            $this->passwordError = 'Tú nombre o una fracción de él no puede ser parte de la contraseña';
+        } elseif (!$this->encontrarPalabra($apellido, $clave)) {
+            $this->passwordError = 'Tú apellido o una fracción de él no puede ser parte de la contraseña';
+        } elseif (!$t)
     }
-
-
 }
-
-?>
