a3138b4931e78b3ab7cb923d8daf14641aa58f86
10Oliver <oliver.erazo1@gmail.com>
2022-10-09 19:10:31 -0600

    Nueva dirección de url

--- controllers/components.js
+++ controllers/components.js
@@ -6,7 +6,7 @@
  *  Ruta del servidor
  */
 //const SERVER = "http://localhost/ProCulinaryShop-2/api/";
-const SERVER = "http://proculinaryshop.herokuapp.com/api/";
+const SERVER = "https://proculinaryshop.herokuapp.com/api/";
 
 /*
  * Función que carga las tablas en las vistas
